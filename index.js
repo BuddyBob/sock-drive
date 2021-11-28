9fa4acb9caa9d3118fd681d52e0e74bacc63995a
Thavas Antonio <airstalk3r@gmail.com>
2021-11-28 10:00:23 -0800

    deploy

--- index.js
+++ index.js
@@ -44,6 +44,33 @@ app.post("/payment",(req,res) => {
 })
 //listen
 
+const whitelist = ['http://localhost:3000', 'http://localhost:8282', 'https://sock-drive.herokuapp.com/ ']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
+
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+
 const port = process.env.PORT || 8282
 
 app.listen(port, () => console.log("Listening at port 8282"))
